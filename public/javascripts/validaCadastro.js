244f76225a031189a88f790a9698a0420a0ee101
Veloso-dev <velosoarts@gmail.com>
2022-07-22 15:40:43 -0300

    resolvendo problemas de validação

--- public/javascripts/validaCadastro.js
+++ public/javascripts/validaCadastro.js
@@ -5,20 +5,26 @@ let email = document.getElementById('email')
 let validaEmail = false
 
 let senha = document.getElementById('senha')
-let regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
+let msgError = document.getElementById('msgError')
 let validaSenha = false
 
 let senhaConfg = document.getElementById('senhaConfg')
 let validaSenhaConfg = false
 
+let msgCad = document.getElementById('msg-cad')
+let msgerro = document.getElementById('msg-erro')
+
 
 nome.addEventListener('keyup' , () => {
     if(nome.value.length <= 2){
         nome.setAttribute('style' , 'color:red')
+
+        validaNome = false
         
 
     }else{
         nome.setAttribute('style' , 'color:green')
+        validaNome = true
     }
 })
 
@@ -40,9 +46,15 @@ email.addEventListener('keyup' , () => {
     }
 })
 
+
+
+
+
 senha.addEventListener('keyup' , () => {
-    if(senha.value.length <= 2 ){
+    if(senha.value.length <= 4 ){
         senha.setAttribute('style' , 'color:red')
+        msgError.setAttribute = ('style', 'display: none;')
+        msgError.innerHTML = 'Insira no minimo 6 caracteres'
         
 
 
@@ -51,14 +63,72 @@ senha.addEventListener('keyup' , () => {
 
     }else{
       senha.setAttribute('style' , 'color:green')
-
-
+      msgError.setAttribute = ('style', 'display: none;')
+      msgError.innerHTML = ''
+      
       return validaSenha = true
     }
 })
 
-    function valida (){
-        setTimeout(()=>{
-            window.location.href = '/index'
-        }, 1000) 
+senhaConfg.addEventListener('keyup' , () =>{
+    if(senha.value != senhaConfg.value){
+        senhaConfg.setAttribute('style' , 'color:red')
+        msgError.setAttribute = ('style', 'display: none;')
+        msgError.innerHTML = 'Senhas não conferem'
+
+        validaSenhaConfg = false
+
+    }else{
+
+        senhaConfg.setAttribute('style' , 'color:green')
+        msgError.setAttribute = ('style', 'display: none;')
+        msgError.innerHTML = ''
+        validaSenhaConfg = true
+
     }
+
+})
+
+
+
+
+    function valida (){
+        if(validaNome && validaEmail && validaSenha && validaSenhaConfg) {
+
+            msgCad.setAttribute('style' , 'display:block')
+            msgCad.innerHTML = 'Cadastrando'
+
+            
+            msgerro.setAttribute('style' , 'display:none')
+            
+            setTimeout(()=>{
+                window.location.href = '/'
+            }, 1000) 
+            
+            return
+
+        }else{
+            
+            msgerro.setAttribute('style' , 'display:block')
+            msgerro.innerHTML = 'Existe campo em branco'
+
+            setTimeout(()=>{
+                window.location.href = '/index'
+            }, 1000) 
+            
+            return
+
+
+            
+        }
+
+
+        }
+
+
+
+
+
+
+
+       
